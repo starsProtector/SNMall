b4a375744514fd794571ef0c6e7f30b4df66db24
starsProtector <starsprotector@163.com>
2017-04-25 16:43:06 +0800

    1.使用OKhttpUtil进行网络请求 2.使用FastJson解析获取到的数据 3.在HomeFragment使用RecyclerView,设置适配器等操作

--- app/src/main/java/com/sn/snmall/home/fragment/HomeFragment.java
+++ app/src/main/java/com/sn/snmall/home/fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.sn.snmall.home.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +10,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.sn.snmall.R;
 import com.sn.snmall.base.BaseFragment;
+import com.sn.snmall.home.adapter.HomeFragmentAdpter;
+import com.sn.snmall.home.bean.ResultBeanData;
+import com.sn.snmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  * date:2017/4/24
@@ -22,7 +31,6 @@ import com.sn.snmall.base.BaseFragment;
  * 4.使用okhttp工具类,完成网络请求数据
  * 5.配置一个常量类Constants,复制存放URL网址
  * 6.使用fastjson解析数据,把数据装入ResultBeanData这个Bean类中
- *
  */
 
 public class HomeFragment extends BaseFragment {
@@ -31,6 +39,8 @@ public class HomeFragment extends BaseFragment {
     private ImageButton ib_top;
     private TextView tv_search_home;
     private TextView tv_message_home;
+    private ResultBeanData.ResultBean mResultBean;
+    private HomeFragmentAdpter mHomeFragmentAdpter;
 
     @Override
     public View initView(LayoutInflater iflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,10 +49,9 @@ public class HomeFragment extends BaseFragment {
         rvHome = (RecyclerView) view.findViewById(R.id.rv_home);
         ib_top = (ImageButton) view.findViewById(R.id.ib_top);
         tv_search_home = (TextView) view.findViewById(R.id.tv_search_home);
-        tv_message_home=(TextView) view.findViewById(R.id.tv_message_home);
+        tv_message_home = (TextView) view.findViewById(R.id.tv_message_home);
         //给按钮设置点击事件
         initListener();
-
         return view;
     }
 
@@ -72,10 +81,68 @@ public class HomeFragment extends BaseFragment {
         });
     }
 
+
     @Override
-    public void initData(){
+    public void initData() {
+        //使用okhttp工具类get请求网络
+        okhttpGetData();
+    }
+
+    //使用okhttp工具类get请求
+    private void okhttpGetData() {
+        //设置请求的网址
+        String url = Constants.HOME_URL;
+        OkHttpUtils
+                .get()//设置请求网络的方式
+                .url(url)//设置请求网址
+                .build()//构建对象
+                //执行请求操作
+                .execute(new StringCallback() {
+            //请求网络失败回调
+            @Override
+            public void onError(Call call, Exception e, int i) {
+                Toast.makeText(mContext, "网络请求失败,么么哒", Toast.LENGTH_SHORT).show();
+            }
+            //请求网络成功回调,使用fastJson进行Json的解析
+            @Override
+            public void onResponse(String s, int i) {
+                //测试是否请求网络成功,及线程是否允许在此更新UI,也就是判断这里是否是主线程
+                Toast.makeText(mContext, "请求网络成功了,哈哈哈哈!", Toast.LENGTH_SHORT).show();
+//                Log.d("YCF",s);
+                //解析Json数据
+                processData(s);
+            }
+        });
 
     }
+
+    //Json解析数据的方法
+    private void processData(String jsonData) {
+        //使用fastJson解析数据,把整理好的数据放入指定的容器中    参数:1.json的数据   2.容器类中的字节码
+        ResultBeanData resultBeanData = JSON.parseObject(jsonData, ResultBeanData.class);
+        //得到容器中装数据的集合
+        mResultBean = resultBeanData.getResult();
+        //测试是否解析Json数据成功
+        String name = mResultBean.getHot_info().get(0).getName();
+//        Log.d("DS",name);
+
+        //B.对mResultBean判断对象是否为空
+        if(mResultBean != null ){
+            //有数据,创建RecyclerView的适配器,    参数:1.上下文   2.数据
+            mHomeFragmentAdpter = new HomeFragmentAdpter(mContext, mResultBean);
+            //RecycleView设置适配器
+            rvHome.setAdapter(mHomeFragmentAdpter);
+            //提示:使用Re测一测设置布局管理者,决定RecyclerView的整体面貌.    参数:1.上下文   2.决定面貌为一行
+            rvHome.setLayoutManager(new GridLayoutManager(mContext,1));
+
+        }else{
+            Toast.makeText(mContext, "没有数据,所以显示为null", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
 
 
 }
---
+++ app/src/main/java/com/sn/snmall/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.sn.snmall.utils;
+
+/**
+ * date:2017/4/25
+ * author:易宸锋(dell)
+ * function:配置各个页面联网的地址
+ */
+
+public class Constants {
+    //服务器原始地址
+    private static String URLBASE="http://169.254.53.96:8080";
+    //主页面的路径
+    public static String HOME_URL = URLBASE +"/DSShop/json/HOME_URL.json";
+
+}
